cb5fad65eff37332b20178b5bc3bc073a5d78e56
thiru-47 <thirumaha042006@gmail.com>
2026-01-10 19:00:01 +0530

    Code for a Basic Linear Search (Modified code to take user input)

--- SMP/src/Linear_Search.java
+++ SMP/src/Linear_Search.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Linear_Search {
     static int method(int[] arr, int target){
         for (int i=0;i<arr.length;i++){
@@ -7,9 +9,19 @@ public class Linear_Search {
         }
         return -1;
     }
-    public static void main(String[] args) {
-        int arr[] = {2,3,4,5,6,7};
-        int target = 6;
+    public static void main(String[] args ) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int size = input.nextInt();
+        System.out.print("Enter the elements of the array : ");
+        int[] arr = new int[size];
+        for (int j=0;j<size;j++){
+            arr[j] = input.nextInt();
+        }
+        System.out.println("Enter the target element : ");
+                int target = input.nextInt();
+        //int arr[] = {2,3,4,5,6,7};
+        //int target = 6;
         int result = method(arr, target);
         if (result == -1){
             System.out.println("Not Found");
